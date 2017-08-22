d04e9d27d37b80d72cfeb6932248190622474688
lv <771633433@qq.com>
2017-08-22 15:39:52 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Page from '@/components/Page'
-import Foo from '@/components/Foo'
-import Bar from '@/components/Bar'
 
 Vue.use(Router)
 
 export default new Router({
   mode:'history',
   routes: [
-    {path: '/',component: Page},
-    {path:'/foo',component:Foo},
-    {path:'/bar',component:Bar}
+    {path: '/',component: Page}
   ]
 })
 
